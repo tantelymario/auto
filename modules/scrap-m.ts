fbce65b12eae7ba4732661d3c821083fca2574cb
root <root@DESKTOP-IN1DPIL>
2023-07-10 12:27:42 +0300

    Création de la nouvelle class

--- modules/scrap-m.ts
+++ modules/scrap-m.ts
@@ -1,20 +1,12 @@
-class Person {
-    name: string;
-    age: number;
+class Auto {
+    
   
-    constructor(name: string, age: number) {
-      this.name = name;
-      this.age = age;
-    }
-  
-    greet(): void {
-      console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
-    }
+  constructor() {
+    console.log(`Auto created`)
   }
   
-  // Create an instance of the Person class
-  const john = new Person('John', 25);
-  
-  // Call the greet method on the instance
-  john.greet();
-  
+   
+}
+export {
+  Auto
+}
---
+++ robot.ts
@@ -0,0 +1,4 @@
+import { Auto } from "./modules/scrap-m";
+
+const auto = new Auto()
+console.log('Fin')
