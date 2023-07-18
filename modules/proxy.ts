31ee2bbdad3761c6a087f448d65fe1166beb253d
tantelymario <tantelymariojs@gmail.com>
2023-07-18 11:12:05 +0600

    axios

--- modules/proxy.ts
+++ modules/proxy.ts
@@ -1,54 +1,37 @@
-import http from 'http';
-import https from 'https';
-import ProxyAgent from 'proxy-agent';
+import axios from 'axios';
+
 
 export class Proxy {
     private ip:string
     private port:number
+    private prot:string
 
-    constructor(ip:string, port:number){
-        this.ip = ip
-        this.port = port
+    constructor(){
+      this.ip = ""
+      this.port = 0
+      this.prot = "" 
     }
-}/*
-const proxyHost = 'proxy-hostname-or-ip';
-const proxyPort = 'proxy-port';
 
-const proxyUrl = `http://${proxyHost}:${proxyPort}`;
-
-const agent = new http.Agent({ 
-    proxy: {
-      host: proxyHost,
-      port: Number(proxyPort),
-    },
-  });
-
-const options = {
-  hostname: 'example.com',
-  path: '/',
-  method: 'GET',
-  agent: agent,
-};
-
-// Use either http or https module based on the URL protocol
-const protocol = options.protocol === 'https:' ? https : http;
-
-const req = protocol.request(options, (res) => {
-  let data = '';
-
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  res.on('end', () => {
-    console.log('Response Code:', res.statusCode);
-    console.log('Response Body:', data);
-  });
-});
-
-req.on('error', (error) => {
-  console.error(error);
-});
-
-req.end();
-*/
+    public check(ip:string, port:number, prot:string): Promise<any>{
+      this.ip = ip
+      this.port = port
+      this.prot = prot
+      
+      return new Promise(async(resolve)=>{
+        const axiosInstance = axios.create({
+          proxy: {
+            host: this.ip,
+            port: this.port,
+          },
+        });
+        try {
+          const response = await axiosInstance.get("http://monip.org/");
+          resolve(response.data);
+          // Handle the response data
+        } catch (error) {
+          resolve(error);
+          // Handle the error
+        }
+      });
+    }
+}
